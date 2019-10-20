96b9eae003626e1e87944c804f04a6c7bc777506
knight-of-design <knight.of.design@gmail.com>
2019-10-20 16:11:47 -0700

    Update tooling

--- tests/awesomeness.ts
+++ tests/awesomeness.ts
@@ -2,7 +2,7 @@ import { describe } from "ava-describe"
 import truly from "../source/awesomeness"
 
 describe('Truly', {
-    'it exports a function': (t, vars) => {
+    'it exports a function': (t) => {
         t.true(truly instanceof Function, 'truly needs to be exported as a function')
     }
 });
