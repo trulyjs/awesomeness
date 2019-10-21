62faa5bdb350e60a10e27e0bd3947a51b4d6b0a5
knight-of-design <knight.of.design@gmail.com>
2019-10-20 21:32:48 -0700

    Develop basic boolean operators

--- built/awesomeness.d.ts
+++ built/awesomeness.d.ts
@@ -1,2 +1,12 @@
-declare const truly: () => void;
+declare class Expression {
+    context: any;
+    constructor(subject: any);
+}
+declare class Truly extends Expression {
+    constructor(subject?: any);
+    and(condition: boolean): this;
+    or(condition: boolean): this;
+    then(): any;
+}
+declare const truly: (subject: any) => Truly;
 export default truly;
--- library/awesomeness.ts
+++ library/awesomeness.ts
@@ -1,16 +1,74 @@
-console.log('Coming soon!')
-
-// Exclude tests in production build
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
     const describe = require("ava-describe").describe
-    describe('Truly', {
-        'it exports a function': (t) => {
-            t.true(truly instanceof Function, 'truly needs to be exported as a function')
+    describe("Truly expression class", {
+        "can be constructed": (t) => {
+            t.truthy(new Truly())
+            t.truthy(new Truly(true))
+            t.truthy(new Truly(false))
+        },
+
+        "can return the inner or provided value with 'then'": (t) => {
+            t.true(new Truly(true).then())
+            t.true(new Truly(true).then())
+            t.is(new Truly(true).then("success"), "success")
+            t.is(new Truly(false).then("error"), null)
+        },
+
+        "can handle the logical operation 'A and B'": (t) => {
+            t.true(new Truly(true).and(true).then())
+            t.false(new Truly(false).and(true).then())
+            t.false(new Truly(true).and(false).then())
+            t.false(new Truly(false).and(false).then())
+        },
+
+        "can handle the logical operator 'A or B'": (t) => {
+            t.true(new Truly(true).or(true).then())
+            t.true(new Truly(true).or(false).then())
+            t.true(new Truly(false).or(true).then())
+            t.false(new Truly(false).or(false).then())
+        },
+    })
+
+}
+
+class Truly  {
+    context: any
+
+    constructor(subject?) {
+        this.context = subject
+    }
+
+    and(condition: boolean): this {
+        this.context = this.context && condition
+        return this
+    }
+
+    or(condition: boolean): this {
+        this.context = this.context || condition
+        return this
+    }
+
+    then(result?: any): any {
+
+        if (typeof result !== "undefined") {
+            this.context = this.context ? result : null;
+        }
+
+        return this.context
+    }
+}
+
+if (process.env.NODE_ENV !== "production") {
+    const describe = require("ava-describe").describe
+    describe("Truly module", {
+        "exports a function": (t) => {
+            t.true(truly instanceof Function, "truly needs to be exported as a function")
         }
     })
 }
 
-const truly = function() {
+const truly = function(subject) {
+    return new Truly(subject)
 }
 
 export default truly
